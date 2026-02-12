3c05ee77616feb37a7c2cdd52a351e7401da42ba
swafey-karanja <kswafey@gmail.com>
2026-02-12 14:17:33 +0300

    updated postion and colours

--- src/components/layout/SocialSidebar.jsx
+++ src/components/layout/SocialSidebar.jsx
@@ -9,61 +9,68 @@ import {
 
 const SocialSidebar = () => {
   return (
-    <div className="fixed right-0 top-1/2 -translate-y-1/2 z-50 flex flex-col">
+    <div className="fixed left-0 top-1/2 -translate-y-1/2 z-10 flex flex-col">
       <a
         href="https://www.facebook.com/share/1Ay79xHcfi/"
-        className="bg-blue-600 p-3 lg:p-5"
+        className="bg-[#1877F2] p-3 lg:p-4"
         target="_blank"
+        rel="noopener noreferrer"
       >
-        <FaFacebookF className="text-white text-sm lg:text-2xl" />
+        <FaFacebookF className="text-white text-sm lg:text-xl" />
       </a>
 
       <a
         href="https://x.com/iGASummit?t=N8kO_9qtwaBu3YeUUaBQNA&s=09"
-        className="bg-black p-3 lg:p-5"
+        className="bg-black p-3 lg:p-4"
         target="_blank"
+        rel="noopener noreferrer"
       >
-        <FaXTwitter className="text-white text-sm lg:text-2xl" />
+        <FaXTwitter className="text-white text-sm lg:text-xl" />
       </a>
 
       <a
         href="https://www.instagram.com/igasummit?igsh=MXF6YXdpYXRxdzBoaQ=="
-        className="bg-[#FD1D1D] p-3 lg:p-5"
+        className="bg-gradient-to-tr from-[#F58529] via-[#DD2A7B] to-[#8134AF] p-3 lg:p-4"
         target="_blank"
+        rel="noopener noreferrer"
       >
-        <FaInstagram className="text-white text-sm lg:text-2xl" />
+        <FaInstagram className="text-white text-sm lg:text-xl" />
       </a>
 
       <a
         href="https://www.linkedin.com/company/igasummit/"
-        className="bg-blue-500 p-3 lg:p-5"
+        className="bg-[#0A66C2] p-3 lg:p-4"
         target="_blank"
+        rel="noopener noreferrer"
       >
-        <FaLinkedin className="text-white text-sm lg:text-2xl" />
+        <FaLinkedin className="text-white text-sm lg:text-xl" />
       </a>
 
       <a
         href="https://www.youtube.com/@iGASummit"
-        className="bg-red-500 p-3 lg:p-5"
+        className="bg-[#FF0000] p-3 lg:p-4"
         target="_blank"
+        rel="noopener noreferrer"
       >
-        <FaYoutube className="text-white text-sm lg:text-2xl" />
+        <FaYoutube className="text-white text-sm lg:text-xl" />
       </a>
 
       <a
         href="https://open.spotify.com/show/11m2XkXyP3MmjHRgXEVgwx?si=8cb7aaae5d2a47bc"
-        className="bg-green-600 p-3 lg:p-5"
+        className="bg-[#1DB954] p-3 lg:p-4"
         target="_blank"
+        rel="noopener noreferrer"
       >
-        <FaSpotify className="text-white text-sm lg:text-2xl" />
+        <FaSpotify className="text-white text-sm lg:text-xl" />
       </a>
 
       <a
-        href="https://open.spotify.com/show/11m2XkXyP3MmjHRgXEVgwx?si=8cb7aaae5d2a47bc"
-        className="bg-blue-700/80 p-3 lg:p-5"
+        href="https://t.me/YOUR_TELEGRAM_LINK"
+        className="bg-[#229ED9] p-3 lg:p-4"
         target="_blank"
+        rel="noopener noreferrer"
       >
-        <FaTelegram className="text-white text-sm lg:text-2xl" />
+        <FaTelegram className="text-white text-sm lg:text-xl" />
       </a>
     </div>
   );
